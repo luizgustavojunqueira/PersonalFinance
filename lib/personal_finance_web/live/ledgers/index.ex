b752f7ddd1092e00708b8d3e4bc5393e86f82045
Luiz Gustavo Junqueira <luizgustavossj@gmail.com>
2025-11-23 15:02:34 -0400

    fix: ledger popup

--- lib/personal_finance_web/live/ledgers/index.ex
+++ lib/personal_finance_web/live/ledgers/index.ex
@@ -1,5 +1,6 @@
 defmodule PersonalFinanceWeb.LedgersLive.Index do
   alias PersonalFinance.Finance
+  alias PersonalFinance.Finance.Ledger
   use PersonalFinanceWeb, :live_view
 
   @impl true
@@ -19,62 +20,36 @@ defmodule PersonalFinanceWeb.LedgersLive.Index do
   end
 
   @impl true
-  def handle_event("open_modal", %{"modal" => modal}, socket) do
-    modal_atom = String.to_existing_atom(modal)
-    {:noreply, assign(socket, open_modal: modal_atom, transaction: nil)}
-  end
-
-  @impl true
-  def handle_event("open_edit_modal", %{"id" => ledger_id}, socket) do
-    current_scope = socket.assigns.current_scope
-
-    ledger = PersonalFinance.Finance.get_ledger(current_scope, ledger_id)
-
-    if ledger do
-      {:noreply,
-       assign(socket,
-         open_modal: :edit_ledger,
-         ledger: ledger
-       )}
-    else
-      {:noreply, put_flash(socket, :error, "Ledger not found.")}
-    end
-  end
-
-  @impl true
-  def handle_event("open_delete_modal", %{"id" => ledger_id}, socket) do
-    current_scope = socket.assigns.current_scope
-    ledger = PersonalFinance.Finance.get_ledger(current_scope, ledger_id)
-
-    if ledger do
-      {:noreply,
-       assign(socket,
-         open_modal: :delete_ledger,
-         ledger: ledger
-       )}
-    else
-      {:noreply, put_flash(socket, :error, "Ledger not found.")}
-    end
+  def handle_params(params, _uri, socket) do
+    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
   end
 
   @impl true
   def handle_event("close_modal", _, socket) do
-    {:noreply, assign(socket, open_modal: nil, transaction: nil)}
+    {:noreply,
+     socket
+     |> assign(open_modal: nil, ledger: nil)
+     |> push_patch(to: ~p"/ledgers")}
   end
 
   @impl true
   def handle_event("delete", %{"id" => id}, socket) do
     current_scope = socket.assigns.current_scope
 
-    ledger = PersonalFinance.Finance.get_ledger(current_scope, id)
-
-    case PersonalFinance.Finance.delete_ledger(current_scope, ledger) do
-      {:ok, _deleted} ->
+    with %Ledger{} = ledger <- Finance.get_ledger(current_scope, id),
+         {:ok, _deleted} <- Finance.delete_ledger(current_scope, ledger) do
+      {:noreply,
+       socket
+       |> assign(ledger: nil, open_modal: nil)
+       |> put_flash(:info, "Ledger excluído com sucesso.")
+       |> stream_delete(:ledger_collection, ledger)
+       |> push_patch(to: ~p"/ledgers")}
+    else
+      nil ->
         {:noreply,
          socket
-         |> assign(ledger: nil, open_modal: nil)
-         |> put_flash(:info, "Ledger excluído com sucesso.")
-         |> stream_delete(:ledger_collection, ledger)}
+         |> put_flash(:error, "Ledger não encontrado.")
+         |> push_patch(to: ~p"/ledgers")}
 
       {:error, _changeset} ->
         {:noreply,
@@ -91,7 +66,40 @@ defmodule PersonalFinanceWeb.LedgersLive.Index do
     {:noreply,
      socket
      |> put_flash(:info, "Ledger salvo com sucesso.")
-     |> stream_insert(:ledger_collection, ledger)
-     |> assign(open_modal: nil)}
+      |> stream_insert(:ledger_collection, ledger, at: 0, replace: true)
+      |> assign(open_modal: nil, ledger: nil)
+     |> push_patch(to: ~p"/ledgers")}
   end
+
+  defp apply_action(socket, :index, _params) do
+    assign(socket, open_modal: nil, ledger: nil, page_title: "Ledgers")
+  end
+
+  defp apply_action(socket, :new, _params) do
+    assign(socket, open_modal: :new_ledger, ledger: nil, page_title: "Novo Ledger")
+  end
+
+  defp apply_action(socket, action, %{"id" => id}) when action in [:edit, :delete] do
+    current_scope = socket.assigns.current_scope
+
+    case Finance.get_ledger(current_scope, id) do
+      %Ledger{} = ledger ->
+        assign(socket,
+          open_modal: modal_name(action),
+          ledger: ledger,
+          page_title: page_title(action, ledger)
+        )
+
+      nil ->
+        socket
+        |> put_flash(:error, "Ledger não encontrado.")
+        |> push_patch(to: ~p"/ledgers")
+    end
+  end
+
+  defp modal_name(:edit), do: :edit_ledger
+  defp modal_name(:delete), do: :delete_ledger
+
+  defp page_title(:edit, ledger), do: "Editar Ledger - #{ledger.name}"
+  defp page_title(:delete, ledger), do: "Excluir Ledger - #{ledger.name}"
 end
